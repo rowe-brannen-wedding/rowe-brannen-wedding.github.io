a9f34c5a6482c941d4c894617735408f4c8bbbad
Jason Rowe <jrowe35813@gmail.com>
2017-09-26 19:31:13 -0400

    querying browser type rather than screen width

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
 const app = {}
-
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+    document.body.style.background = "url('../resources/background-960.png') no-repeat center center fixed"
+}
 app.description = 'Jason and Kathleen\'s Wedding Site!'
