a7e1c40419216682c0ebde26dd9a6955bb71c507
Jason Rowe <jrowe35813@gmail.com>
2017-09-26 20:16:45 -0400

    verifying js is running correctly on mobile

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 const app = {}
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
     document.body.style.background = "url('../resources/background-960.png') no-repeat center center fixed"
+    // document.body.style.height = "100vh"
+    document.getElementById("under-construction").innerHTML = "Thanks for visiting from a mobile browser!"
 }
 app.description = 'Jason and Kathleen\'s Wedding Site!'
